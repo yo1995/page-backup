40197169760bf5f62c4d86eba48a4acc1c494de8
cht-laptop <cht@bupt.edu.cn>
2016-09-12 15:37:40 +0800

    上传照片 20160912

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 cd 'C:/Program Files/Git/usr/yo1995.github.io/photos/page-backup'
 
 git add .
-git commit -m '上传照片 $(date +%Y%m%d)'
+git commit -m "上传照片 $(date +%Y%m%d)"
 git push
 read -p "部署完毕，任意键退出！" qu
